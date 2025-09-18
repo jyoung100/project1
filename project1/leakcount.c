04d8d72bda8e00ec8a089bc2adf078bd72a5acb2
jyoung100 <jyoung9890@gmail.com>
2025-09-18 08:21:00 -0400

    Project1 Calloc, Realloc, Free

--- project1/leakcount.c
+++ project1/leakcount.c
@@ -7,9 +7,19 @@
 int main(int argc, char **argv)
 {
   	int *mem = (int*)(malloc(10));
+
+	void *p = malloc(12);
+	void *x = malloc(100);
+	void *c = (int*)(calloc(50, sizeof(int)));
+	void *d = malloc(7);
 	int *mem1 = (int*)(malloc(30));
-	void *p = malloc(10);
-	//printf("Mem[0] Address: %p\n", mem);
-	//printf("Mem1[0] Address: %p\n", mem1);
-	//printf("Malloc Calls: %d\n", malloc_count);
+
+	int*mem2 = (int*)(calloc(5, sizeof(int)));
+	
+	free(mem2);
+
+	realloc(mem, 20);
+	return 0;
+
 }
+
--- project1/mem_shim.c
+++ project1/mem_shim.c
@@ -3,29 +3,115 @@
 #include <dlfcn.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+
+#define max_allocations 10000
 
 
-int malloc_count = 0;
-int resolving = 0;
 void *(*original_malloc) (size_t size) = NULL;
+void *(*original_calloc) (size_t, size_t) = NULL;
+void (*original_free) (void* ptr);
+void *(*original_realloc) (void* ptr, size_t size) = NULL;
+
+typedef struct{
+	void *ptr;
+	size_t size;
+} allocations;
+
+int extra = 0;
+int leakcount = 0;
+int bytes = 0;
+int resolving = 0;
+
+allocations table[max_allocations];
+
+int insert(void *p, size_t size){
+	for(int i = 0; i < max_allocations; i++){
+		if(table[i].ptr == p) {table[i].size = size; return 1;}
+	}
+
+	for(int i = 0; i < max_allocations; i++){
+		if(table[i].ptr == NULL){
+			table[i].ptr = p; 
+			table[i].size = size;  
+			bytes+= size;
+			return 1;}
+	}
+	return 0;
+}
+
+int delete(void *p){
+for(int i = 0; i < max_allocations; i++){
+	if(table[i].ptr == p){
+		table[i].ptr = NULL;
+		bytes-=table[i].size;
+		table[i].size = 0;
+		--leakcount;
+		return 1;
+	}
+
+}
+}
+
 
 void __attribute__((destructor)) library_cleanup() {
-	printf("Number of calls to malloc: %d\n", malloc_count);
-	printf("Unloading library.\n");
-}
+	
+	for(int j = 0; j < max_allocations; j++){
+		if(table[j].ptr != NULL){
+			dprintf(STDERR_FILENO, "Leak\t%zu\n", table[j].size);
+		}
+	}
+	dprintf(STDERR_FILENO, "Total %zu %zu\n", (size_t)leakcount, (size_t)bytes);
 
+/*	for(int j = 0; j < max_allocations; j++){
+		if(table[j].ptr != NULL){
+			printf("Leak\t%ld\n", table[j].size);
+		}}
+	printf("Total %d %d\n", leakcount, bytes-1024);
+*/
+}
 
 void *malloc(size_t size){
-    if(!original_malloc){
-        if(resolving)
-            return NULL;
-    	resolving = 1;
+	if(!original_malloc){
+		if(resolving)
+			return NULL;
+		resolving = 1;
 		original_malloc = dlsym(RTLD_NEXT , "malloc");
-    	resolving = 0;
-    	if(!original_malloc)
-        	return NULL;
-    }
-	++malloc_count;
-	return original_malloc(size);
+		resolving = 0;
+		if(!original_malloc)
+			return NULL;
+	}
+	++leakcount;
+	void* ptr = original_malloc(size);
+	insert(ptr, size);
+	return ptr;
 }
+
+void *calloc(size_t count, size_t size){
+	original_calloc = dlsym(RTLD_NEXT, "calloc");
+	++leakcount;
+	void* ptr = original_calloc(count, size);
+	insert(ptr, size*count);
+	return ptr;
+}
+
+void free(void *ptr){
+   original_free = dlsym(RTLD_NEXT, "free");
+   delete(ptr);
+   --leakcount;
+   return;
+   }
+
+void *realloc(void *ptr, size_t size){
+	original_realloc = dlsym(RTLD_NEXT, "realloc");
+	//delete(ptr);
+	insert(ptr, size);
+	return;
+}
+
+
+
+
+   
+
 
